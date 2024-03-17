5fff9941b438240d8edf505f6f2977acad03ec0f
Filip Stamenkovic <fstamenkovic26@gmail.com>
2024-03-17 12:12:05 +0100

    Created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { BodyComponent } from './components/body/body.component';
+import { FormTaskComponent } from './components/form-task/form-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    BodyComponent,
+    FormTaskComponent
   ],
   imports: [
     BrowserModule,
